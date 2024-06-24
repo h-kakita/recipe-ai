15bdc68e50b4824e4dc0aac2251d09e0e399441b
h.kakita <h.kakita@lincrea.co.jp>
2024-06-24 16:25:27 +0900

    コード修正

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,18 +1,18 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { data } from "./data/resource";
-import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { defineBackend } from '@aws-amplify/backend';
+import { data } from './data/resource';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 const backend = defineBackend({
   data,
 });
 
 const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
-  "bedrockDS",
-  "https://bedrock-runtime.us-east-1.amazonaws.com",
+  'bedrockDS',
+  'https://bedrock-runtime.us-east-1.amazonaws.com',
   {
     authorizationConfig: {
-      signingRegion: "us-east-1",
-      signingServiceName: "bedrock",
+      signingRegion: 'us-east-1',
+      signingServiceName: 'bedrock',
     },
   }
 );
@@ -20,9 +20,8 @@ const bedrockDataSource = backend.data.resources.graphqlApi.addHttpDataSource(
 bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
   new PolicyStatement({
     resources: [
-      "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0",
+      'arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-3-haiku-20240307-v1:0',
     ],
-    actions: ["bedrock:InvokeModel"],
-    
+    actions: ['bedrock:InvokeModel'],
   })
 );
